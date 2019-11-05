f5a5443f8e16ec4bd454d6b6ade625a6edd36877
Ayşe Güven <ayse.ylmz.78@gmail.com>
2019-11-05 10:20:45 +0300

    Initialize project

---
+++ UserInputValidation/UserInputValidation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserInputValidation
+//
+//  Created by Ayşe Güven on 5.11.2019.
+//  Copyright © 2019 Ayşe Güven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
